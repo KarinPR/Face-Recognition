a9a17a63f8e9cc5820f435947019432f7df005d5
Karin Povolozki-Rabichev <karinpovolozki512@gmail.com>
2019-06-18 20:10:10 +0100

    Added explenation

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -8,6 +8,12 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 			<p className = 'f3'>
 				{'This Magic Brain will detect faces in your pictures. Give it a try.'}
 			</p>
+			<p className = 'f5 serif'>
+				{' Just enter an image URL with a face'}  
+			</p>
+			<p className = 'f5 serif'>
+				{'You can use this URL to start - " https://www.faceplusplus.com/scripts/demoScript/images/demo-pic4.jpg " '}
+			</p>
 			<div className = 'br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw8 center'>
 				<div className = 'form center pa4 br3 shadow-5'>
 					<input className = 'f4 pa2 w-70 br-pill center' type = 'text' onChange = {onInputChange}/>
